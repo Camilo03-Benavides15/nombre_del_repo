9746f620ba1c3d999d492cf53c96a8a7748e1de7
Camilo03-Benavides15 <LUISCAMILOORTIZ41@GMAIL.COM>
2025-03-31 22:52:57 -0500

    Add files via upload

---
+++ Estudiante.java
@@ -0,0 +1,24 @@
+package GestionDeNotas.model;
+
+public class Estudiante {
+    private int id;
+    private String nombre;
+    private String codigo;
+
+    public Estudiante(int id,String nombre, String codigo){
+        this.id = id;
+        this.nombre = nombre;
+        this.codigo = codigo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public String getCodigo() {
+        return codigo;
+    }
+}
